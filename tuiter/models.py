e9f16f8396aced8526af1825772c32e55f83e0f0
jhgakiyama <lp_japo@hotmail.com>
2022-02-08 08:10:49 -0300

    agrego el modelo 'Perfil'

--- tuiter/models.py
+++ tuiter/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Perfil(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE)
+
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        # permite acceder a las entradas de datos desde el otro extremo
+        # de esa relación a través del nombre "followed_by".
+        blank=True,
+        symmetrical=False
+        # el user 1 puede seguir al user 2,pero el user 2 -> no sigue al user1
+        # https://docs.djangoproject.com/en/4.0/ref/models/fields/#django.db.models.ManyToManyField.symmetrical
+
+    )
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = "Perfiles"
