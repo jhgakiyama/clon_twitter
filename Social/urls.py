4f64462d49e3e568623a2237fb7168e63ab69d9a
jhgakiyama <lp_japo@hotmail.com>
2022-02-09 00:38:30 -0300

    agrego base.html

--- Social/urls.py
+++ Social/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import DashboardView
 
 urlpatterns = [
+    path("", include("tuiter.urls")),
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path('admin/', admin.site.urls),
 ]
