bfb6939d1ec2d39287bdce291266c9101560aa1c
jhgakiyama <lp_japo@hotmail.com>
2022-02-12 01:19:44 -0300

    utilizo home.html como template

--- Social/views.py
+++ Social/views.py
@@ -2,4 +2,7 @@ from django.views.generic import TemplateView
 
 
 class DashboardView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'home.html'
+    #
+    # def get_context_data(self, **kwargs):
+    #     object =
