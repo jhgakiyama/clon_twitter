5e8665b7090665845ab0ca330191048bf423a71d
jhgakiyama <lp_japo@hotmail.com>
2022-02-17 22:08:05 -0300

    fix en el get_query_set

--- Social/views.py
+++ Social/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import ListView
 from tuiter.models import Tuit
-from django.contrib.auth.models import User
 
 
 class HomeTuitsListView(ListView):
@@ -9,10 +8,12 @@ class HomeTuitsListView(ListView):
 
     def get_queryset(self):
         """
-        Recupero todos los tuist , de los usuarios que yo sigo, excluyo mis tuits
+        Recupero todos los tuits , de los Perfiles que sigue el User autenticado
+        Se excluyen los tuits del User autenticado
         """
+
         if self.request.user.is_authenticated:
-            return Tuit.objects.filter(user__in=User.objects.filter(
-                id__in=self.request.user.perfil.follows.all())).exclude(user=self.request.user)
+            return Tuit.objects.filter(
+                user__perfil__in=self.request.user.perfil.follows.all()).exclude(user=self.request.user)
 
         return None
