9c6315042ea150bdc28b8562aac72b6a643124e9
jhgakiyama <lp_japo@hotmail.com>
2022-02-25 22:04:14 -0300

    cambio vista para ver un perfil

--- tuiter/urls.py
+++ tuiter/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PerfilListView, PerfilDetailView
+from .views import PerfilListView, detailperfil
 
 urlpatterns = [
     # path("", dashboard, name="dashboard"),
     path("perfil_list/", PerfilListView.as_view(), name="perfil_list"),
-    path("perfil/<int:pk>", PerfilDetailView.as_view(), name="perfil_detail"),
+    path("perfil/<int:pk>", detailperfil, name="perfil_detail"),
 ]
--- tuiter/views.py
+++ tuiter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .models import Perfil
 
 
@@ -13,5 +13,24 @@ class PerfilListView(ListView):
         return Perfil.objects.all().order_by('user__username')
 
 
-class PerfilDetailView(DetailView):
-    model = Perfil
+def detailperfil(request, pk):
+    context = {}
+    perfil_actual = Perfil.objects.get(pk=pk)
+    template_name = "tuiter/perfil_detail.html"
+
+    if request.method == "POST":
+        mi_perfil = request.user.perfil
+        data = request.POST
+        accion = data.get("btn_seguir")  # obtengo el 'value' del boton
+
+        if accion == "seguir":
+            mi_perfil.follows.add(perfil_actual)
+        elif accion == "no_seguir":
+            mi_perfil.follows.remove(perfil_actual)
+
+        mi_perfil.save()
+
+    context["object"] = perfil_actual
+
+    return render(request, template_name, context)
+
